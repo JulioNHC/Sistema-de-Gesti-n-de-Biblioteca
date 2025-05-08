b22ae8f1ee8ab0a683d6879f9247ff7b7a94b71d
JulioNHC <julionahumhernandez@gmail.com>
2025-05-07 21:54:27 -0600

    Implementacion de todo el backend

---
+++ Node/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const config = require('./config');
+
+const clientes = require('./modulos/Clientes/rutas');
+const error = require ('./red/errors')
+
+const app = express();
+
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//configuracion
+app.set('port', config.app.port)
+
+//Rutas
+app.use('/api/clientes', clientes)
+app.use(error);
+
+module.exports = app;
---
+++ Node/src/modulos/Clientes/controlador.js
@@ -0,0 +1,22 @@
+const db = require('../../DB/mysql');
+
+const TABLA = 'miembros';
+
+function todos(){
+    return db.todos(TABLA);
+}
+
+function uno(id){
+    return db.uno(TABLA, id);
+}
+
+function agregar(body){
+    return db.agregar(TABLA, body);
+}
+
+function eliminar(body){
+    return db.eliminar(TABLA, body);
+}
+
+
+module.exports ={todos,uno,agregar,eliminar}
