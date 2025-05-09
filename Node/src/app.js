44dccb4eaa3298370495e842b26386fea627577b
JulioNHC <julionahumhernandez@gmail.com>
2025-05-09 11:54:13 -0600

    MOSTRAR API

--- Node/src/app.js
+++ Node/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const config = require('./config');
+const cors = require('cors');
 
 const clientes = require('./modulos/Clientes/rutas');
 const error = require ('./red/errors')
 
 const app = express();
-
+app.use(cors());
 
 //Middleware
 app.use(morgan('dev'));
